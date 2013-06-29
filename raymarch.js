5d24338fc20973f09034cd12375bf4d5b6c69eed
Nicolas Silva <nical.silva@gmail.com>
2013-06-29 12:35:07 +0200

    fix

--- raymarch.js
+++ raymarch.js
@@ -1,12 +1,12 @@
 function updateRaymarch(program) {
   var mat = mat4.create();
   var m2 = mat4.create();
-  
-  var position = [D.clipTime/100.0,10.0,15.0];
 
-  mat4.rotate(m2, mat, D.currentTime/10000.0, [0,0,0])
-  updateTimeUniforms();
-  gl.uniformMatrix4fv(gl.getUniformLocation(D.currentProgram[0], 'mvmat'), false, m2);
-gl.uniform3fv(gl.getUniformLocation(D.currentProgram[0], 'position'),
+  var position = [D.clipTime/100.0,15.0,15.0];
+
+  mat4.rotate(m2, mat, D.currentTime/100000.0, [0,0,0])
+  updateTimeUniforms(program);
+  gl.uniformMatrix4fv(gl.getUniformLocation(program, 'mvmat'), false, m2);
+  gl.uniform3fv(gl.getUniformLocation(program, 'position'),
                 new Float32Array(position));
 }
