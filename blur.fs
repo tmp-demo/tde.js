5f81f6234b9dfccb03d1dda1de558b1830d685b3
Paul ADENOT <paul@paul.cx>
2013-06-29 10:57:13 +0800

    meh

--- blur.fs
+++ blur.fs
@@ -5,20 +5,20 @@ uniform float time;
 uniform float duration;
 /* resolution */
 uniform vec2 res;
+uniform float beat;
 
 uniform sampler2D u_image;
 uniform vec2 u_textureSize;
-
 varying vec2 v_texCoord;
 
-void main() {
-
-  float col = (duration - time) / duration;
-  float coef = 4.0;
-  vec4 result = vec4(0.0,0.0,0.0,0.0);
-  result = result + texture2D(u_image,v_texCoord );
-  gl_FragColor= result / coef; 
-	
-  
+vec2 fisheye(vec2 coord, float amt) {
+  vec2 cc = coord - 0.5;
+  float dist = dot(cc, cc);
+  return coord + cc * dist * amt;
 }
 
+void main(void)
+{
+  float amount = beat * 5.0;
+  gl_FragColor = texture2D(u_image, fisheye(v_texCoord * 0.7 + 0.15, amount));
+}
