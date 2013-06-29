5d41cd0178b6f93d3b3692edb3f2e130ef2ba7e6
Nicolas Silva <nical.silva@gmail.com>
2013-06-29 17:07:19 +0200

    stuff

--- chroma.fs
+++ chroma.fs
@@ -19,11 +19,16 @@ void main(void)
   vec4 narmol = texture2D(u_image,vec2(uv.x, uv.y));
 
   float displaced = 0.0;
-  if (beat > 0.60) {
-    displaced = beat;
+  if (beat > 0.75) {
+    vec2 fc = vec2(gl_FragCoord.x, gl_FragCoord.y);
+    float dist = length(fc - res);
+    vec2 center = vec2(res.x / 2.0, res.y/2.0);
+    float distcenter = abs(length(fc - center));
+    displaced = 0.1 + beat * distcenter / length(res) + sin(time);
   }
 
-  vec4 redmoved = texture2D(u_image, vec2(uv.x + 4.0 * displaced / res.x, uv.y));
+
+  vec4 redmoved = texture2D(u_image, vec2(uv.x + 18.0 * displaced / res.x, uv.y));
 
   gl_FragColor = vec4(redmoved.r, narmol.g, narmol.b, 1.0);
 }
--- scenes.js
+++ scenes.js
@@ -5,6 +5,7 @@ D.scenes.pushScene = function(scene) {
 }
 
 function loadScenes() {
+  // intro
   D.scenes.pushScene( {
     duration: 5000,
     fragments: ["city_intro"],
@@ -14,27 +15,34 @@ function loadScenes() {
     }]
   });
 
+  // sad
   D.scenes.pushScene( {
-    duration: 5000,
+    duration: 3000,
     fragments: ["city_1"],
     vertex: "quad",
     update: [function(prog) {
-      updateRaymarchTranslate(prog, [0, 15.0, 15.0],[10, 10.0, 15.0]);
+      updateRaymarchTranslate(prog, [0, 15.0, 15.0],[20, 10.0, 15.0]);
     }]
   });
 
+  // rainbow!
   D.scenes.pushScene( {
-    duration: 15000,
+    duration: 3000,
     fragments: ["city_rainbow"],
     vertex: "quad",
-    update: [updateRaymarch]
+    update: [function(prog) {
+      updateRaymarchTranslate(prog, [20, 10.0, 15.0],[20, 20.0, -100.0]);
+    }]
   });
 
+  // traveling right with chroma
   D.scenes.pushScene( {
     duration: 15000,
     fragments: ["city_2", "chroma"],
     vertex: "quad",
-    update: [updateRaymarch, updateDefault]
+    update: [function(prog) {
+      updateRaymarchTranslate(prog, [20, 15.0, 15.0],[100, 15.0, 15.0]);
+    }, updateDefault]
   });
 
   D.scenes.pushScene( {
