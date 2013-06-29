b2d3d11f5c131806dcf13ec56b6ba2ec98c35b64
Nicolas Silva <nical.silva@gmail.com>
2013-06-29 12:31:48 +0200

    city shader

--- raymarch.js
+++ raymarch.js
@@ -3,11 +3,11 @@ function updateRaymarch() {
   var mat = mat4.create();
   var m2 = mat4.create();
   
-  var position = [D.clipTime/100.0,15.0,15.0];
+  var position = [D.clipTime/100.0,10.0,15.0];
 
-  mat4.rotate(m2, mat, D.currentTime/100000.0, [0,0,1])
+  mat4.rotate(m2, mat, D.currentTime/10000.0, [0,0,0])
   updateTimeUniforms();
   gl.uniformMatrix4fv(gl.getUniformLocation(D.currentProgram[0], 'mvmat'), false, m2);
-  gl.uniform3fv(gl.getUniformLocation(D.currentProgram[0], 'position'),
+gl.uniform3fv(gl.getUniformLocation(D.currentProgram[0], 'position'),
                 new Float32Array(position));
-}
+}currentProgram
