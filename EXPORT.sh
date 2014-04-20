09093b1aec1170cabb60f0876f3e49ebb0e7afa0
Remi Papillie <remi.papillie@gmail.com>
2014-04-21 01:57:06 +0200

    moved embedded pnginator

--- EXPORT.sh
+++ EXPORT.sh
@@ -25,11 +25,6 @@ fi
 echo " -- running the closure compiler..."
 java -jar tools/compiler.jar --js=./export/demo.js --js_output_file=./export/demo_min.js --compilation_level=ADVANCED_OPTIMIZATIONS --externs ./externs/w3c_audio.js
 
-if [ ! -f tools/pnginator.rb ]; then
-    echo " -- tools/pnginator.rb not found, now downloading it..."
-    wget -O tools/pnginator.rb "https://gist.githubusercontent.com/gasman/2560551/raw/f589cb80cca2ad7a7169cb75a2b7f3e03be16c4d/pnginator.rb"
-fi
-
 echo " -- packing in a png..."
 ruby tools/pnginator.rb export/demo_min.js export/demo.png.html
 
