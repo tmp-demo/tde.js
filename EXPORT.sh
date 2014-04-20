6da5e44b5ff053a13a843c3379809d6f481e701b
Remi Papillie <remi.papillie@gmail.com>
2014-04-20 18:00:47 +0200

    added automatic download for crunching tools when they are not found locally

--- EXPORT.sh
+++ EXPORT.sh
@@ -5,6 +5,7 @@ mkdir -p ./export/assets
 rm -rf ./export/*.js
 rm -rf ./export/*.fs
 rm -rf ./export/*.vs
+mkdir -p tools
 
 echo " -- concatenating js files and stripping debug code..."
 
@@ -21,11 +22,22 @@ cp -r ./assets/*.png ./export/assets/
 cp -r ./assets/*.jpg ./export/assets/
 cp -r ./assets/*.ogg ./export/assets/
 
+if [ ! -f tools/compiler.jar ]; then
+    echo " -- tools/compiler.jar not found, now downloading it..."
+    wget -O tools/compiler-latest.zip "http://dl.google.com/closure-compiler/compiler-latest.zip"
+    cd tools && unzip compiler-latest.zip
+fi
+
 echo " -- running the clojure compiler..."
-java -jar compiler.jar --js=./export/demo.js --js_output_file=./export/demo_min.js --compilation_level=ADVANCED_OPTIMIZATIONS --externs ./export/externs
+java -jar tools/compiler.jar --js=./export/demo.js --js_output_file=./export/demo_min.js --compilation_level=ADVANCED_OPTIMIZATIONS --externs ./export/externs
+
+if [ ! -f tools/pnginator.rb ]; then
+    echo " -- tools/pnginator.rb not found, now downloading it..."
+    wget -O tools/pnginator.rb "https://gist.githubusercontent.com/gasman/2560551/raw/f589cb80cca2ad7a7169cb75a2b7f3e03be16c4d/pnginator.rb"
+fi
 
 echo " -- packing in a png..."
-ruby pnginator.rb export/demo_min.js export/demo.png.html
+ruby tools/pnginator.rb export/demo_min.js export/demo.png.html
 
 echo " -- done."
 wc -c *.js
