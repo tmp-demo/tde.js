1af4474b3e7cf03d3cde07680b64c0f1f73b6a64
U-WSMIND-LAPTOP\wsmind <wsmind@wsmind-laptop.(none)>
2014-05-01 19:10:17 +0200

    implemented shader minification pipeline

---
+++ shaders/_common.glsl
@@ -0,0 +1,31 @@
+//! COMMON
+
+// TODO the hard-coded far plane is a bit lame
+#define FAR_DIST 50.0
+
+precision lowp float;
+
+uniform mat4 view_proj_mat;
+
+uniform vec2 resolution;
+
+uniform sampler2D texture_0;
+uniform sampler2D texture_1;
+uniform sampler2D texture_2;
+uniform sampler2D texture_3;
+uniform sampler2D texture_4;
+
+uniform vec2 step;
+
+varying vec2 v_tex_coords;
+varying vec3 v_normals;
+varying vec3 v_position;
+
+//! VERTEX
+attribute vec3 position;
+attribute vec3 normals;
+attribute vec2 tex_coords;
+
+//! FRAGMENT
+#extension GL_EXT_draw_buffers : require
+#define NB_BLUR_TAPS 10.0
