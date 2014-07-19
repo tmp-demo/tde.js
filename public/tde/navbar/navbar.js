7451d0c7eb101cb0bb36312b4ef8513b5ac1c808
Remi Papillie <remi.papillie@gmail.com>
2014-07-19 16:17:01 +0200

    implemented basic login box

--- public/tde/navbar/navbar.js
+++ public/tde/navbar/navbar.js
@@ -3,12 +3,38 @@ angular.module("tde.navbar", [])
 .controller("NavbarCtrl", function($scope, User)
 {
   $scope.logout = User.logout
+  
+  $scope.login = function()
+  {
+    User.login($scope.name, $scope.email)
+  }
 })
 
 .directive("tdeNavbar", function()
 {
   return {
     restrict: "E",
-    templateUrl: "/tde/navbar/navbar.html"
+    templateUrl: "/tde/navbar/navbar.html",
+    link: function($scope)
+    {
+      var dialogShown = false
+      
+      $scope.$watch("currentUser.name", function(name)
+      {
+        if (!name && !dialogShown)
+        {
+          $("#loginBox").modal({
+            backdrop: "static",
+            keyboard: false
+          })
+          dialogShown = true
+        }
+        else if (name && dialogShown)
+        {
+          $("#loginBox").modal("hide")
+          dialogShown = false
+        }
+      })
+    }
   }
 })
