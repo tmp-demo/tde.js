4308568885be5b2c03f86e697e55a6de2f311b8e
Remi Papillie <remi.papillie@gmail.com>
2014-08-03 20:12:54 +0200

    removed old stuff

--- engine_tde_old/shaders_old/quad.vs
+++
@@ -1,7 +0,0 @@
-attribute vec2 position;
-varying vec2 v_tex_coords;
-
-void main() {
-  gl_Position = vec4(position, 0.0, 1.0);
-  v_tex_coords = (vec2(1.0, 1.0) + position) / 2.0;
-}
--- engine_tde_old/shaders_old/show_normals.fs
+++
@@ -1,8 +0,0 @@
-precision lowp float;
-uniform sampler2D texture_0;
-varying vec2 v_tex_coords;
-varying vec3 v_normals;
-
-void main() {
-  gl_FragColor = vec4(0.5*v_normals+vec3(0.5,0.5,0.5), 1.0);
-}
