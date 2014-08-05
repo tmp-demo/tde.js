b743d972553598a6a262926a473b4b7e3c8c34da
Jonathan Giroux <bloutiouf@gmail.com>
2014-08-05 21:16:54 +0200

    Refactor text textures

--- public/shaders/printtext.glsl
+++ public/shaders/printtext.glsl
@@ -2,7 +2,7 @@
 
 void main_vs_printtext() {
   gl_Position = vec4(position.xy, 0.0, 1.0);
-  v_tex_coords = (position.xy * 0.5 + 0.5 - (text_params.xy - text_params.zw * 0.5)) / text_params.zw;
+  v_tex_coords = ((position.xy + text_params.zw + 1.0) * 0.5 - text_params.xy) / text_params.zw;
 }
 
 //! FRAGMENT
