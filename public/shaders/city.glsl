60ed162e6ca3ccc95a381a5d8a250cb7d450239f
Nicolas Silva <nical.silva@gmail.com>
2014-08-05 23:08:10 +0200

    tweak the city vertex shader to make the world less flat

--- public/shaders/city.glsl
+++ public/shaders/city.glsl
@@ -1,7 +1,9 @@
 //! VERTEX
 
 void main_vs_city() {
-  gl_Position = view_proj_mat * vec4(position, 1.0);
+  gl_Position = view_proj_mat * vec4(position
+    + vec3(0.0, sin(position.x * position.z / 10000.0)*20.0, 0.0),
+   1.0);
   v_position = position;
   v_normals = normals;
   v_tex_coords = tex_coords;
