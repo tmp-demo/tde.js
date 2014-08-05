96316c88dbe3497ae3b01701c1b5f60894758937
Nicolas Silva <nical.silva@gmail.com>
2014-08-05 22:53:30 +0200

    buildings shading

--- public/engine/geom.js
+++ public/engine/geom.js
@@ -184,11 +184,12 @@ function shrink_path(path, amount, z, use_subdiv) {
 
 function fill_convex_ring(geom, ring) {
   var normal = [0, 1, 0];
-  var uv = [0.5, 0.9];
+  // roof top or grass
+  var uv = ring[0][1] > 10 ? [0.5, 0.9] : [0.6, 0.05];
   for (var i = 1; i < ring.length - 1; i++) {
       push_vertices(geom.positions, [ring[0], ring[i], ring[i + 1]]);
       push_vertices(geom.normals, [normal, normal, normal]);
-      push_vertices(geom.uvs, [[0.5, 0.9], uv, uv]);
+      push_vertices(geom.uvs, [uv, uv, uv]);
   }
 }
 
--- public/shaders/buildings_mtl.glsl
+++ public/shaders/buildings_mtl.glsl
@@ -25,25 +25,29 @@ float rectangle(vec2 uv, float x1, float y1, float x2, float y2) {
 //   |      +-+        |
 //   |      | |  wall  |
 //   |      +-+        |
-//   +-----------------+ 0.2
-//   |     street      |
-// 0 +-----------------+ 0.0
-//   0
+//   +--------+--------+ 0.2
+//   | street |  grass |
+// 0 +--------+--------+ 0.0
+//   0.0     0.5
+//
 void main_fs_buildings_mtl() {
   vec2 uv = v_tex_coords;
 
   float window    = rectangle(uv, 0.4, 0.35, 0.6, 0.50);
   float wall      = rectangle(uv, 0.0, 0.2,  1.0, 0.8) - window;
   float roof      = rectangle(uv, 0.0, 0.8,  1.0, 1.0);
-  float street    = rectangle(uv, 0.0, 0.0,  1.0, 0.1);
+  float street    = rectangle(uv, 0.0, 0.0,  0.5, 0.1);
   float side_walk = rectangle(uv, 0.0, 0.1,  1.0, 0.2);
+  float grass     = rectangle(uv, 0.5, 0.0,  1.0, 0.1);
 
   float wall_shade = uv.y * 0.3 + 0.6;
 
-  vec3 color = window * vec3(0.0, 0.0, 0.0)
-             + roof   * vec3(0.2, 0.2, 0.4) * sin(uv.x*1000.0)
-             + wall   * vec3(wall_shade, wall_shade*0.9, wall_shade*0.7)
-             + street * vec3(0.3, 0.3, 0.3)
+  vec3 color = window    * vec3(0.0, 0.0, 0.0)
+             + wall      * vec3(wall_shade, wall_shade*0.9, wall_shade*0.7)
+             + roof      * vec3(0.2, 0.2, 0.4) * sin(uv.x*1000.0)
+             + grass     * vec3(0.0, 0.6, 0.1)
+             + street    * vec3(0.3, 0.3, 0.3)
              + side_walk * vec3(0.5, 0.5, 0.5);
+
   gl_FragColor = vec4(color, 1.0);
 }
