3a26cff10e8ed53605eb016967136278e60132aa
Paul Adenot <paul@paul.cx>
2014-08-09 00:03:44 +0200

    better anim interpolation

--- public/engine/anim.js
+++ public/engine/anim.js
@@ -1,17 +1,7 @@
-function animate(keyframes, time)
+function animate(p0, p1, p2,t)
 {
-  var last = keyframes.length - 1;
-  if (time <= keyframes[0][0]) return keyframes[0][1];
-  if (time >= keyframes[last][0]) return keyframes[last][1];
-  
-  // we must have at least 2 keyframes, or it will crash
-  var i = 0;
-  while ((i < last) && (keyframes[i][0] < time)) i++;
-  var k1 = keyframes[i - 1];
-  var k2 = keyframes[i];
-  var t = (time - k1[0]) / (k2[0] - k1[0]);
-  return k1[1].map(function(v, i)
-  {
-    return (1 - t) * v + t * k2[1][i];
-  })
+  for (var i = 0; i < p0.length; i++) {
+    p0[i] = (p0[i] - 2 * p1[i] + p2[i]) * t * t + 2 * (p1[i] - p0[i]) * t + p0[i];
+  }
+  return p0;
 }
