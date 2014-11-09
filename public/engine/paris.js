f3a4a6c8d02fdf35549882e1bee27294b51a2d15
Nicolas Silva <nical.silva@gmail.com>
2014-11-09 17:35:30 +0100

    Added paris.js

---
+++ public/engine/paris.js
@@ -0,0 +1,76 @@
+
+function paris_subdivision_rec(paths, num_subdivs, sub_id) {
+    if (sub_id < 0) { sub_id = 0; }
+    var sub_paths = [];
+    for (var i in paths) {
+        var sub = city_subdivision(paths[i], sub_id)
+        if (!sub) {
+            sub_paths.push(paths[i]);
+        }
+        else {
+            sub_paths.push(sub[0], sub[1]);
+        }
+    }
+    if (num_subdivs == 1) {
+        return sub_paths;
+    }
+    return city_subdivision_rec(sub_paths, num_subdivs - 1, sub_id - 1);
+}
+
+
+// TODO make this show in the editor: it defines how the min size of city blocks
+var MIN_PERIMETER = 260;
+
+function paris_subdivision(path, sub_id) {
+    var path_length = path.length;
+
+    // a1 is the index of the point starting the first edge we'll cut.
+    // b1 is the index of the point starting the second edge we'll cut.
+    var a1;
+    var maxd = 0;
+    var perimeter = 0;
+    var i; // loop index, taken out to win a few bytes
+    // pick the longest segment
+    for (i = 0; i < path_length; ++i) {
+        var d = vec2.distance(path[i], path[(i+1)%path_length]);
+        if (d > maxd) {
+            maxd = d;
+            a1 = i;
+        }
+        perimeter += d;
+    }
+
+    if (perimeter < MIN_PERIMETER) { return null; }
+
+    var a2 = (a1+1) % path_length;
+    var b1, b2;
+
+    do {
+        b1 = rand_int(path_length);
+        if (a1 == b1 || a1 == b1 + 1) { continue; }
+
+        b2 = (b1+1) % path_length;
+
+        var f1 = 0.5 + (0.5 - M.abs(seedable_random() - 0.5)) * 0.2;
+        var f2 = 0.5 + (0.5 - M.abs(seedable_random() - 0.5)) * 0.2;
+
+        var p_a3_1 = { '0': path[a1][0]*f1 + path[a2][0]*(1.0-f1), '1': path[a1][1]*f1 + path[a2][1]*(1-f1), subdiv: sub_id};
+        var p_a3_2 = { '0': path[a1][0]*f1 + path[a2][0]*(1.0-f1), '1': path[a1][1]*f1 + path[a2][1]*(1-f1), subdiv: path[a1].subdiv};
+        var p_b3_1 = { '0': path[b1][0]*f2 + path[b2][0]*(1.0-f2), '1': path[b1][1]*f2 + path[b2][1]*(1-f2), subdiv: sub_id};
+        var p_b3_2 = { '0': path[b1][0]*f2 + path[b2][0]*(1.0-f2), '1': path[b1][1]*f2 + path[b2][1]*(1-f2), subdiv: path[b1].subdiv};
+
+        break;
+    } while (1);
+
+    var path1 = [p_a3_1, p_b3_2]
+    for (i = b2; i != a2; i = mod((i+1), path_length)) {
+        path1.push(path[i]);
+    }
+
+    var path2 = [p_b3_1, p_a3_2]
+    for (i = a2; i != b2; i = mod((i+1), path_length)) {
+        path2.push(path[i]);
+    }
+
+    return [path1, path2];
+}
