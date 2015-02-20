d0b8b93ea10c691624a4d0d5e782985e5d2b02c5
Remi Papillie <remi.papillie@gmail.com>
2015-02-20 21:03:59 +0100

    more import

--- tools/obj2tde.py
+++ tools/obj2tde.py
@@ -17,26 +17,39 @@ def load_materials(filename):
                 currentMaterial = {}
                 materials[fragments[1]] = currentMaterial
             elif fragments[0] == "Kd":
-                currentMaterial["diffuse"] = fragments[1:4]
+                currentMaterial["diffuse"] = (float(fragments[1]), float(fragments[2]), float(fragments[3]))
 
         return materials
 
 def load_mesh(filename):
     with open(filename) as meshFile:
+        materials = {}
+        positions = []
+        normals = []
+
+        currentMaterial = None
+
         for line in meshFile:
             fragments = line.split()
 
             if len(fragments) < 2:
                 continue
 
-            materials = {}
-            vertices = []
-
             if fragments[0] == "mtllib":
                 materials = load_materials(os.path.join(os.path.dirname(filename), fragments[1]))
-                print(materials)
+            elif fragments[0] == "v":
+                positions.append((float(fragments[1]), float(fragments[3]), -float(fragments[2])))
+            elif fragments[0] == "vn":
+                normals.append((float(fragments[1]), float(fragments[3]), -float(fragments[2])))
+            elif fragments[0] == "usemtl":
+                currentMaterial = materials[fragments[1]]
+            elif fragments[0] == "f":
+                if len(fragments) > 4:
+                    # make 2 triangles out of a quad
+                    fragments[1:] = fragments[1:4] + fragments[2:5]
+                    # todo: split on '/'
 
-        return None
+        return positions
 
 if __name__ == "__main__":
     print(load_mesh("../data/particles/raw/TORI-GATETDF.obj"))
