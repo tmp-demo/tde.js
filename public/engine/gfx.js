bcf1a3d700027afc7a8dd8e16fb363dbac7f9f07
Nicolas Silva <nical.silva@gmail.com>
2015-02-21 15:25:59 +0900

    support variable clear color

--- public/engine/gfx.js
+++ public/engine/gfx.js
@@ -347,7 +347,7 @@ function render_scene(scene, demo_time, scene_time) {
     }
     
     if (pass.clear) {
-      gl.clearColor(1.0, 1.0, 1.0, 1.0);
+      gl.clearColor(pass.clear[0], pass.clear[1], pass.clear[2], pass.clear[3]);
       gl.clearDepth(1.0);
       gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
     }
