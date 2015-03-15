ee702c9a3bac67fa4446d65bf8bf8aeb0efbab3f
Nicolas Silva <nical.silva@gmail.com>
2015-03-15 15:39:19 +0100

    Fix scenes support

--- public/engine/gfx.js
+++ public/engine/gfx.js
@@ -403,16 +403,27 @@ function render_pass(pass, time) {
         gl.clearDepth(1.0);
         gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
       }
-      
-      if (pass.scenes) {
-        var scene = scenes[pass.scene];
+
+      if (pass.scene) {
+        // A scene can be inlined in the sequence...
+        var scene = pass.scene;
+        if (typeof(scene) == "string") {
+          // ...or in its own asset
+          scene = scenes[pass.scene];
+        }
+
+        // This allows us to inline the object list without the other members of the scene
+        // for convenience and space.
+        //    scenes: { objects: [{geometry: "quad"}] },
+        // is quivalent to:
+        //    scenes: [{geometry: "quad"}],
+        var scene_objects = scene.objects || scene;
+
         send_uniforms(shader_program, scene.uniforms, clip_time);
 
-        for (var g = 0; g < scene.objects.length(); ++g) {
-          var obj = scene.objects[i]
+        for (var g = 0; g < scene_objects.length; ++g) {
+          var obj = scene_objects[g];
           var geometry = geometries[obj.geometry];
-
-          send_uniforms(shader_program, obj.uniforms, clip_time);
 
           //#debug{{
           if (!geometry) {
@@ -420,6 +431,12 @@ function render_pass(pass, time) {
             geometry = geometry_placeholder
           }
           //#debug}}
+
+          // this is optional, but can be a convenient info to have in the shader.
+          obj.uniforms = obj.uniforms || {};
+          obj.uniforms["u_object_id"] = g;
+
+          send_uniforms(shader_program, obj.uniforms, clip_time);
 
           draw_geom(geometry)
         }
